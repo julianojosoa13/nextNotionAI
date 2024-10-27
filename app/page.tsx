1937fdd885331adc5250d313166fbac17655c562
julianojosoa13 <josoajuliano@gmail.com>
2024-10-27 16:11:01 +0300

    Shadcn and Clerk setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <h1>Let's build an AI Notion App</h1>
+      <Button>Click Me</Button>
     </main>
   );
 }
