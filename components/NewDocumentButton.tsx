213dffb78530a703fcaf7127a7da25ad808387bf
julianojosoa13 <josoajuliano@gmail.com>
2024-10-28 13:38:06 +0300

    Sidebar

---
+++ components/NewDocumentButton.tsx
@@ -0,0 +1,6 @@
+import { Button } from "./ui/button";
+
+const NewDocumentButton = () => {
+  return <Button>New Document</Button>;
+};
+export default NewDocumentButton;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import NewDocumentButton from "./NewDocumentButton";
+
+const Sidebar = () => {
+  return (
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+      <NewDocumentButton />
+    </div>
+  );
+};
+export default Sidebar;
